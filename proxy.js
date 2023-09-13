4029e634b63655bcc3e09a921a0ef2fe73f053aa
GVishnudhasan <gvishnud10@gmail.com>
2023-09-13 19:00:59 +0530

    update import in proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { URLSearchParams } = require('url');
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = 3005;
